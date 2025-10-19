201a6d9f53cd5449362bc5d15a116b27fb6dfd5b
pratikkabra143 <pratikkabra2000@gmail.com>
2025-10-19 07:42:18 +0530

    initial xperience changes

--- xperience_courbet.mk
+++ xperience_courbet.mk
@@ -15,10 +15,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from courbet device
 $(call inherit-product, device/xiaomi/courbet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Xperience stuff.
+$(call inherit-product, vendor/xperience/config/common.mk)
 
-PRODUCT_NAME := lineage_courbet
+PRODUCT_NAME := xperience_courbet
 PRODUCT_DEVICE := courbet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 11 Lite 4G
@@ -26,6 +26,18 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+# XPerience specific
+XPERIENCE_MAINTAINER := pratikkabra143
+XPERIENCE_BATTERY := 4250mah (TYP)
+XPERIENCE_DISPLAY := 1080x2400
+TARGET_STOCK_GAPPS := true
+WITH_GMS:=true
+#TARGET_ESSENTIAL_GAPPS := true
+TARGET_ENABLE_PIXEL_GOODIES := true
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="courbet_global-user 13 TKQ1.221013.002 V14.0.3.0.TKQMIXM release-keys" \
-    BuildFingerprint=Xiaomi/courbet/courbet:13/TKQ1.221013.002/V14.0.3.0.TKQMIXM:user/release-keys
+    BuildFingerprint=Xiaomi/courbet/courbet:13/TKQ1.221013.002/V14.0.3.0.TKQMIXM:user/release-keys\
+    DeviceProduct=courbet \
+    SystemName=courbet_global
+
