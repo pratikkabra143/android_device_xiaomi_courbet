16abe831d102e4741f6e43a5b460577115fa9152
pratikkabra143 <pratikkabra2000@gmail.com>
2025-10-19 07:00:18 +0530

    initial pixelos changes

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_courbet.mk
+    $(LOCAL_DIR)/custom_courbet.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_courbet-user \
-    lineage_courbet-userdebug \
-    lineage_courbet-eng
+    custom_courbet-user \
+    custom_courbet-userdebug \
+    custom_courbet-eng
--- custom_courbet.mk
+++ custom_courbet.mk
@@ -1,6 +1,5 @@
 #
-# Copyright (C) 2021-2025 The LineageOS Project
-#
+# SPDX-FileCopyrightText: The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -15,10 +14,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from courbet device
 $(call inherit-product, device/xiaomi/courbet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_courbet
+PRODUCT_NAME := custom_courbet
 PRODUCT_DEVICE := courbet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 11 Lite 4G
